4113bee77744dae8950b08428d9b43bd2e2d33d4
marlon-buckley <m4rlz@live.co.uk>
2022-12-17 13:38:45 +0000

    removing comments

--- day3/backpackSorter.js
+++ day3/backpackSorter.js
@@ -1,14 +1,7 @@
 const fs = require('fs');
 
-//splits each line of input into an string
 const input = fs.readFileSync('./input.txt', 'utf-8');
 const splitInput = input.split(/\r?\n/); 
-
-//for each line, split in half 
-  //for each half find the chars that appears in both
-    //assign that char a number, a-z 1-26 & A-Z 27-52
-  
-//tally up the total for all the numbers calculated from each line
 
 const findItemCode = (item) => {
   if (item.charCodeAt() >= 97) {
@@ -61,31 +54,10 @@ const part2 = (splitInput) => {
         result += findItemCode(item);
       }
     })
-
   })
 
-
-  console.log(backpacks);
   return result;
 }
 
 console.log(part2(splitInput));
 
-
-
-
-
-  // let chunkSize = 3;
-  // let groups = [];
-  // let uniqueBackPacks = [];
-  // let result = 0;
-
-  // while (splitInput.length > 0) {
-  //   groups.push(splitInput.splice(0, chunkSize));
-  // }
-
-  // groups.forEach((group) => {
-  //   group.forEach((line) => {
-  //     uniqueBackPacks.push([...new Set(line)]); 
-  //   })
-  // })
