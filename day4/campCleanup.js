27bff163eb15662a5b19d720dfae7298eb4d342f
marlon-buckley <marlon.buckley@protonmail.com>
2023-09-29 15:03:36 +0100

    a messy solution to day4 part2

--- day4/campCleanup.js
+++ day4/campCleanup.js
@@ -1,54 +1,63 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync('./input.txt', 'utf-8');
-const splitInput = input.split(/\r?\n/); 
-
+const input = fs.readFileSync("./input.txt", "utf-8");
+const splitInput = input.split(/\r?\n/);
 
 const rangeGenerator = (start, end) => {
-  return Array(end - start + 1).fill().map((_, idx) => start + idx)
+  return Array(end - start + 1)
+    .fill()
+    .map((_, idx) => start + idx);
 };
 
-
-const rangePuller = (splitInput) => {
+const convertInputToArrayOfNumberPairs = (splitInput) => {
   let pairs = [];
   splitInput.forEach((rangePair) => {
     let workingRange = [];
-    let splitPair = rangePair.split(',');
+    let splitPair = rangePair.split(",");
     splitPair.forEach((range) => {
-      let rangeNumbers = range.split('-');
-      let testArr = rangeNumbers.map(str => {
+      let rangeNumbers = range.split("-");
+      let testArr = rangeNumbers.map((str) => {
         return Number(str);
       });
       workingRange.push(testArr);
-    })
+    });
     pairs.push(workingRange);
-  })
-  
-  return pairs
+  });
+
+  return pairs;
 };
 
-
-const compareRanges = (rangePairs) => {
+const part1 = (rangePairs) => {
   let matchCount = 0;
-
   rangePairs.forEach((pair) => {
-    //console.log(pair);
-    let firstRange = rangeGenerator(pair[0][0], pair[0][1])
-    let secondRange = rangeGenerator(pair[1][0], pair[1][1])
-    //console.log(firstRange, secondRange);
-    if (secondRange.every(element => firstRange.includes(element))) {
-      console.log('match found for', 'first range', firstRange, 'second range', secondRange);
-      matchCount ++
+    let firstRange = rangeGenerator(pair[0][0], pair[0][1]);
+    let secondRange = rangeGenerator(pair[1][0], pair[1][1]);
+
+    if (secondRange.every((element) => firstRange.includes(element))) {
+      matchCount++;
+    } else if (firstRange.every((element) => secondRange.includes(element))) {
+      matchCount++;
     }
-    else if (firstRange.every(element => secondRange.includes(element))) {
-      console.log('match found', 'first range', firstRange, "\n", 'second range', secondRange);
-      matchCount ++
+  });
+
+  return `There are ${matchCount} ranges that fully contain another`;
+};
+
+const rangePairs = convertInputToArrayOfNumberPairs(splitInput);
+
+const part2 = (inputPairs) => {
+  let count = 0;
+
+  inputPairs.forEach((pair) => {
+    const firstRange = rangeGenerator(pair[0][0], pair[0][1]);
+    const secondRange = rangeGenerator(pair[1][0], pair[1][1]);
+    if (firstRange.some((item) => secondRange.includes(item))) {
+      count++;
     }
-  })
+  });
 
-  return `There are ${matchCount} ranges that fully contain another`
-}
+  return count;
+};
 
-const rangePairs = rangePuller(splitInput);
-
-console.log(compareRanges(rangePairs));
+console.log(part2(rangePairs));
+// console.log(part1(rangePairs));
