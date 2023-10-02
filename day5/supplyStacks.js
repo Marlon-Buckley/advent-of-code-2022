1faa0969e9f8e5d6d0fe4f7e373ad4d1cc5a563c
marlon-buckley <marlon.buckley@protonmail.com>
2023-10-02 19:30:07 +0100

    count columns aka stacks

--- day5/supplyStacks.js
+++ day5/supplyStacks.js
@@ -1,6 +1,23 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("./example.txt", "utf-8");
-const splitInput = input.split(/\r?\n/);
+const splitInput = input.split(/\n/);
 
-console.log(splitInput);
+let split = [];
+let columnCount = 0;
+
+// how do I know number of columns?
+// by the numbers at the bottom of the stacks
+// so lets grab that line, split to an array
+// then slice the last two elemnts, and grab the first
+// now we have the column count
+
+splitInput.forEach((line) => {
+  if (line.startsWith(" 1")) {
+    columnCount = line.split(" ").slice(-2)[0];
+  }
+  split.push(line);
+});
+
+console.log(columnCount);
+console.log(split);
