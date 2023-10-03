a7dbe5e2949625e650d2393a40bf08706747a546
marlon-buckley <marlon.buckley@protonmail.com>
2023-10-03 21:20:28 +0100

    wrap most logic in functions without breaking anything

--- day5/supplyStacks.js
+++ day5/supplyStacks.js
@@ -2,49 +2,59 @@ import { readFileSync } from "fs";
 const input = readFileSync("./input.txt", "utf-8");
 const splitInput = input.split(/\n/);
 
-let rows = [];
-let columnCount = 0;
-let moves = [];
+const parseInputIntoUsefulThing = (input) => {
+  let rows = [];
+  let stackCount = 0;
+  let moves = [];
 
-splitInput.forEach((line) => {
-  if (line.startsWith(" 1")) {
-    columnCount = line.split(" ").slice(-2)[0];
-  } else if (line.includes("[")) {
-    rows.push(line);
-  } else if (line.startsWith("move")) {
-    moves.push(line);
-  }
-});
-
-let start = 0;
-let end = 3;
-let crates = {};
-
-//populate the crates object
-for (let i = 0; i < columnCount; i++) {
-  const column = rows.map((line) => line.slice(start, end).trim());
-  const tidyColumn = column.filter((crate) => crate);
-  crates[i + 1] = tidyColumn;
-  start += 4;
-  end += 4;
-}
-
-// console.log("crates start out like", JSON.stringify(crates, undefined, 2));
-
-for (let i = 0; i < moves.length; i++) {
-  const instructions = moves[i].match(/\d+/g);
-
-  const numCratesToMove = instructions[0];
-  const stackToTakeFrom = instructions[1];
-  const stackToMoveTo = instructions[2];
-  const crateToMove = crates[stackToTakeFrom].splice(0, numCratesToMove);
-
-  crateToMove.forEach((thing) => {
-    crates[stackToMoveTo].unshift(thing);
+  input.forEach((line) => {
+    if (line.startsWith(" 1")) {
+      stackCount = line.split(" ").slice(-2)[0];
+    } else if (line.includes("[")) {
+      rows.push(line);
+    } else if (line.startsWith("move")) {
+      moves.push(line);
+    }
   });
 
-  console.log("crates now like: ", JSON.stringify(crates, undefined, 2));
-}
+  return { rows, stackCount, moves };
+};
+
+const buildCratesObject = (rows, stackCount) => {
+  let start = 0;
+  let end = 3;
+  let crates = {};
+
+  for (let i = 0; i < stackCount; i++) {
+    const column = rows.map((line) => line.slice(start, end).trim());
+    const tidyColumn = column.filter((crate) => crate);
+    crates[i + 1] = tidyColumn;
+    start += 4;
+    end += 4;
+  }
+
+  return crates;
+};
+
+const moveTheCrates = (crates, moves) => {
+  for (let i = 0; i < moves.length; i++) {
+    const instructions = moves[i].match(/\d+/g);
+
+    const numCratesToMove = instructions[0];
+    const stackToTakeFrom = instructions[1];
+    const stackToMoveTo = instructions[2];
+    const cratesToMove = crates[stackToTakeFrom].splice(0, numCratesToMove);
+
+    cratesToMove.forEach((crate) => {
+      crates[stackToMoveTo].unshift(crate);
+    });
+  }
+};
+
+const { rows, stackCount, moves } = parseInputIntoUsefulThing(splitInput);
+const crates = buildCratesObject(rows, stackCount);
+
+moveTheCrates(crates, moves);
 
 for (const column in crates) {
   console.log(crates[column][0]);
